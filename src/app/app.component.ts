b5863caf931e58fedfe1203f6695997815773486
shrikumar hegde <shrikumarhegde@gmail.com>
2018-10-31 22:52:42 +0530

    github host

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit, AfterViewInit } from '@angular/core';
 import { ItunesService } from './shared/itunes.service';
+import { fromEvent,interval } from 'rxjs';
+import { debounce } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit,AfterViewInit {
+  @ViewChild('searchBox') searchInput: ElementRef;
+  // (keyup)="search(searchBox.value)"
+
+  hideResult:boolean;
   searchResults: Array<any> = [];
   constructor(private ituneService: ItunesService) {}
+
+  ngOnInit(){
+  }
+
+  ngAfterViewInit(){
+    let buttonStream$=fromEvent(this.searchInput.nativeElement, 'keyup')
+    .pipe(debounce(val => interval(1000)))
+    .subscribe(()=>{
+      this.search(this.searchInput.nativeElement.value);
+    });
+
+  }
+
+  onResultClick(){
+    this.hideResult=true;
+    this.searchInput.nativeElement.value='';
+  }
 
   search(param) {
     this.ituneService.search(param).subscribe(
       data => {
         // console.log(data['results']);
+        this.hideResult=false;
         this.searchResults = data['results'];
       },
       err => console.log(err)
